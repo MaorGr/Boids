bc2156c5f05663db4af02dd2950eef2010897937
MaorG <maor.grinberg@gmail.com>
2023-11-06 20:47:05 +0200

    before merge

--- world/Boid.cpp
+++ world/Boid.cpp
@@ -1,204 +1,137 @@
-#include "Boid.h"
-#include "../external/quad/Quadtree.h"
+#include "boid.h"
 #include <SFML/Graphics.hpp> 
-
 #include <iostream> // todo(maor): replace with a logging system
 
-const float DT = 0.01f; //TODO(remove)
-const float MAX_SPEED = 25.0f;
+const float DT = 1.0f; //TODO(remove)
+const float MAX_SPEED = 6.0f;
+const float MIN_SPEED = 3.0f;
 
 
 Boid::Boid(float x, float y, float vx, float vy) : Boid(x, y) {
-    this->velocity = sf::Vector2f(vx, vy);
+    this->velocity = Vector2f(vx, vy);
 }
 
 Boid::Boid(float x, float y) : position(x, y), velocity(0, 0), acceleration(0, 0) {
+    // this->position = Point(x, y);
+    // this->velocity = Point(0, 0);
+    // this->acceleration = Point(0, 0);
     touchRadius = 2.5;
     sensingRadius = 50;
-    box.left = x - touchRadius;
-    box.top = y - touchRadius;
-    box.width = 2.0 * touchRadius;
-    box.height = 2.0 * touchRadius;
+    // this->bbox = Box(Point(x - touchRadius, y - touchRadius), Point(x + touchRadius, y + touchRadius));
+    this->id = this->maxId;
+    this->maxId += 1;
 }
 
-
-float Boid::getX() const {
-    return this->position.x;
+Vector2f Boid::getPosition() {
+    return this->position;
 }
 
-void Boid::setX(float x) {
-    this->position.x = x;
-    this->box.left = x - this->box.width * 0.5f;
+void Boid::setPosition(Vector2f position) {
+    this->position = position;
 }
 
-float Boid::getY() const {
-    return this->position.y;
+Vector2f Boid::getVelocity() {
+    return this->velocity;
 }
 
-void Boid::setY(float y) {
-    this->position.y = y;
-    this->box.top = y - this->box.height * 0.5f;
+void Boid::setVelocity(Vector2f velocity){
+    this->velocity = velocity;
 }
 
-
-quadtree::Box <float> Boid::getBox() const {
-    return this->box;
-}
-
-template <typename GetBoxFunc>
-std::vector<Boid> Boid::getNeighbors(float radius, const quadtree::Quadtree<Boid*, GetBoxFunc>& quadtree) const {
-
-    float x = this->getX();
-    float y = this->getY();
-
-    quadtree::Box qbox = quadtree::Box<float>(x - radius,
-                                                y - radius,
-                                                2.0 * radius,
-                                                2.0 * radius);
-    // std::cout << "[" << qbox.left << ", " << qbox.left + qbox.width << "], [" << qbox.top << ", " << qbox.top + qbox.height << "]" << std::endl; 
-    std::vector<Boid*> neighbors = quadtree.query(qbox);
-    // std::cout << " > " << neighbors.size() << std::endl;
-    std::vector<Boid> ngh = std::vector<Boid>();
-    for (auto neighbor : neighbors) {
-        if ((this->box.left - neighbor->box.left) * (this->box.left - neighbor->box.left) + 
-            (this->box.top - neighbor->box.top) * (this->box.top - neighbor->box.top) < 
-            radius * radius) {
-            ngh.push_back(*neighbor);
-        }
-    }
-    return ngh;
+Box Boid::getBox() {
+    // todo(maor): invalidate?
+    this->bbox = Box(Point(this->position.x() - touchRadius, this->position.y() - touchRadius), Point(this->position.x() + touchRadius, this->position.y() + touchRadius));
+    return this->bbox;
 }
 
 void Boid::updateAvoidanceDirection(const std::vector<Boid>& neighbors) {
-    sf::Vector2f repulsionForce = sf::Vector2f(0, 0); 
-    sf::Vector2f predictiveAvoidance = sf::Vector2f(0, 0);
+    Vector2f repulsion = Vector2f(0, 0); 
+    // Vector2f predictiveAvoidance = Vector2f(0, 0);
 
-    float repulsion_factor = 10.0f;
+    float repulsion_factor = 0.15f;
 
     for (Boid neighbor : neighbors) {
         // Calculate distance to neighbor
-        sf::Vector2f delta = this->position - neighbor.position;
-        float distance = std::sqrt(delta.x * delta.x + delta.y * delta.y); 
+        Vector2f delta = this->getPosition() - neighbor.getPosition();
+        // float distance = delta.norm();
+        repulsion -= delta;
+    }
+    this->velocity += this->velocity + repulsion_factor * repulsion;
 
-        // Repulsion Force
-        if (distance < this->box.width * 0.1) {
-            sf::Vector2f away = delta;  // vector from neighbor to boid
-            away = away * (std::sqrt(away.x * away.x + away.y * away.y));   // Make it a unit vector
-            away /= (distance + 0.0001f);   // Weaker force if further away
-            repulsionForce += away;
-        }
+    
+ 
 
-        // Predictive Avoidance
-        // sf::Vector2f futureBoidPosition = this->position + boid.velocity * PREDICTIVE_LOOKAHEAD;
-        // sf::Vector2f futureNeighborPosition = neighbor->position + neighbor->velocity * PREDICTIVE_LOOKAHEAD;
-
-        // if (futureBoidPosition.distanceTo(futureNeighborPosition) < COLLISION_THRESHOLD) {
-        //     Vector2D avoidanceDir = futureBoidPosition - futureNeighborPosition;
+        // float PREDICTIVE_LOOKAHEAD = 0.1;
+        // float COLLISION_THRESHOLD = 0.1;
+        // // Predictive Avoidance
+        // Eigen::Vector2f futureBoidPosition = this->position + this->velocity * PREDICTIVE_LOOKAHEAD;
+        // Eigen::Vector2f futureNeighborPosition = neighbor.position + neighbor.velocity * PREDICTIVE_LOOKAHEAD;
+        // Eigen::Vector2f delta_future =futureBoidPosition - futureNeighborPosition;
+        // if (delta_future.norm() < COLLISION_THRESHOLD) {
+        //     Eigen::Vector2f avoidanceDir = futureBoidPosition - futureNeighborPosition;
         //     avoidanceDir.normalize();
         //     predictiveAvoidance += avoidanceDir;
         // }
-    }
+    // }
 
     // Combine forces and normalize
-    sf::Vector2f combined = repulsionForce + predictiveAvoidance;
+    // Eigen::Vector2f combined = repulsionForce + predictiveAvoidance;
     // if (combined.length() > 0) {
     //     combined.normalize();
     // std::cout << "old: " << this->velocity.x << ", " << this->velocity.y;
-    this->velocity = this->velocity + repulsion_factor * combined;
+    // this->velocity = this->velocity + repulsion_factor * combined;
     // std::cout << "--> new: " << this->velocity.x << ", " << this->velocity.y << std::endl;
     // this->velocity += 0.000001f * combined;
     // }
 }
 
 
-template <typename GetBoxFunc>
-void Boid::update(const quadtree::Quadtree<Boid*, GetBoxFunc>& quadtree) {
-    float dt = 0.01;
-
-    // std::cout << "old: [" << this->getX() << ", " << this->getY() << "]";  
-
-    if (this->getX() < 0) {
-        this->setX(-1.0 * this->getX());
-        velocity.x = -1.0 * velocity.x;
-    } 
-    if (this->getX() > 800) {
-        this->setX(2 * 800.0 - 1.0 * this->getX());
-        velocity.x = -velocity.x;
-    } 
-    if (this->getY() < 0) {
-        this->setY(-1.0 * this->getY());
-        velocity.y = -1.0 * velocity.y;
-    } 
-    if (this->getY() > 600) {
-        this->setY(2 * 600.0 - 1.0 * this->getY());
-        velocity.y = -1.0 * velocity.y;
-    } 
-
-    // std::cout << " --> new:[" << this->getX() << ", " << this->getY() << "]" << std::endl; 
-
-    std::vector<Boid> touching = this->getNeighbors(this->box.width, quadtree);
-    this->updateAvoidanceDirection(touching);
-
-
-    std::vector<Boid> neighbors = this->getNeighbors(50.0f, quadtree);
-    std::cout << neighbors.size() << std::endl;
+void Boid::doFlocking(std::vector<Boid> &neighbors) {
     auto result = this->getBoidsAvgInfo(neighbors);
+
+    float matchingFactor = 0.05;
+    float centeringFactor = 0.0005;
 
     
     if (result) {
-        sf::Vector2f avgPos = result->first;
-        sf::Vector2f avgVel = result->second;
+        Eigen::Vector2f avgPos = result->first;
+        Eigen::Vector2f avgVel = result->second;
         // Use avgPos and avgVel as needed
 
-        sf::Vector2f deltaPos = avgPos - this->position; 
-        sf::Vector2f deltaVel = avgVel - this->velocity; 
-        float factorAcc = 0.01f;
-        float factorPos = 0.01f;
-
-        sf::Vector2f deltaAcc = factorPos * deltaPos + factorAcc * deltaVel;
-
-        this->acceleration = this->acceleration + deltaAcc;
+        // Eigen::Vector2f deltaAcc = factorP * deltaPos + factorV * deltaVel;
+        // this->acceleration = this->acceleration + deltaAcc;
+        this->velocity += (avgVel) * matchingFactor;
+        this->velocity += (avgPos) * centeringFactor;
     }
-    else {
-        // this->velocity = 0.9f * this->velocity;
-    }
+}
 
+void Boid::update() {
 
     this->velocity = this->velocity + this->acceleration * DT;
-    this->position = this->position + this->velocity * DT;
-
-    float speed = std::sqrt(velocity.x * velocity.x + velocity.y * velocity.y);
-    if (speed > MAX_SPEED) {
-        velocity = (velocity / speed) * MAX_SPEED;
+    if (this->velocity.norm() > MAX_SPEED) {
+        this->velocity = this->velocity / MAX_SPEED; 
+    } else if (this->velocity.norm() > MIN_SPEED){
+        this->velocity = this->velocity / MIN_SPEED;
     }
+    this->position = this->position + this->velocity * DT;
+}
 
-
-    // TODO(remove redundency of box and position)
-    this->setX(this->getX());
-    this->setY(this->getY());
-};
-
-// void draw(sf::RenderWindow& window) {
-//     sf::CircleShape shape(5);  // simple circle for each boid
-//     shape.setPosition(position);
-//     window.draw(shape);
-// }
-
-std::optional<std::pair<sf::Vector2f, sf::Vector2f>> Boid::getBoidsAvgInfo(const std::vector<Boid>& boids) {
-    sf::Vector2f sumPos(0, 0);
-    sf::Vector2f sumVel(0, 0);
+std::optional<std::pair<Vector2f, Vector2f>> Boid::getBoidsAvgInfo(const std::vector<Boid>& boids) {
+    Vector2f sumPos(0, 0);
+    Vector2f sumVel(0, 0);
     int count = 0;
     const float perceptionRadius = 50.0f;
     const float perceptionRadiusSqr = perceptionRadius*perceptionRadius;
 
     for (const auto& other : boids) {
-        sf::Vector2f delta = other.position - this->position;
-        const float dist = std::sqrt(delta.x * delta.x + delta.y * delta.y);
+        Vector2f deltaP = other.position - this->position;
+        Vector2f deltaV = other.velocity - this->velocity;
+        const float dist = deltaP.norm();
         if (dist > 0) {
             float velW = fmax(0, (perceptionRadius - dist) / perceptionRadius);
             float posW = fmax(0, (perceptionRadius - dist) / perceptionRadius);
-            sumPos += posW * delta;
-            sumVel += velW * other.velocity;
+            sumPos += deltaP;
+            sumVel += deltaV;
         }
         
         count++;
@@ -206,8 +139,8 @@ std::optional<std::pair<sf::Vector2f, sf::Vector2f>> Boid::getBoidsAvgInfo(const
     }
     if (count > 0) {
         return std::make_pair(
-            this->position + (sumPos / float(count)), 
-            sumVel / float(count)
+            (sumPos / float(count)), 
+            (sumVel / float(count))
         );
     } else {
         return {};  // No neighbors found
--- world/Boid.h
+++ world/Boid.h
@@ -1,34 +1,40 @@
-#include "../external/quad/Quadtree.h"
-#include <SFML/Graphics.hpp> 
-// todo(maor): remove SFML dependency, switch to standard 2d lib, handle visualization separately 
+#pragma once
+
+#include "geometry.h"
 
 class Boid {
 private:
-    sf::Vector2f position;
-    sf::Vector2f velocity;
-    sf::Vector2f acceleration;
+    unsigned int id; 
+    static int maxId;
+    Eigen::Vector2f position;
+    Eigen::Vector2f velocity;
+    Eigen::Vector2f acceleration;
     float touchRadius;
     float sensingRadius;
+    Box bbox;
 
 public:
     Boid(float x, float y, float vx, float vy);
-
     Boid(float x, float y);
+    Vector2f getPosition();
+    void setPosition(Vector2f position);
+    Vector2f getVelocity();
+    void setVelocity(Vector2f velocity);
+    Box getBox();
+    inline unsigned int getId() { return id; };
 
-    float getX() const;
-    void setX(float x);    
-    float getY() const;
-    void setY(float y);
-    quadtree::Box <float> box;
-    quadtree::Box <float> getBox() const;
+    // quadtree::Box <float> box;
+    // quadtree::Box <float> getBox() const;
     
-    template <typename GetBoxFunc>
-    std::vector<Boid> getNeighbors(float radius, const quadtree::Quadtree<Boid*, GetBoxFunc>& quadtree) const;
+    // template <typename GetBoxFunc>
+    // std::vector<Boid> getNeighbors(float radius, const quadtree::Quadtree<Boid*, GetBoxFunc>& quadtree) const;
 
+    void doFlocking(std::vector<Boid> &neighbors);
     void updateAvoidanceDirection(const std::vector<Boid>& neighbors);
 
-    // template <typename GetBoxFunc>
-    void update(const quadtree::Quadtree<Boid*, GetBoxFunctor>& quadtree);
-    std::optional<std::pair<sf::Vector2f, sf::Vector2f>> getBoidsAvgInfo(const std::vector<Boid>& boids);
+    // // template <typename GetBoxFunc>
+    // void update(const quadtree::Quadtree<Boid*, GetBoxFunctor>& quadtree);
+    void update();
+    std::optional<std::pair<Vector2f, Vector2f>> getBoidsAvgInfo(const std::vector<Boid>& boids);
 
 };
